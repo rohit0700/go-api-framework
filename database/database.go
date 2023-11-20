6bb8fb3157dfc71f19216cf1c3bd12f975feb4a1
rohit_cept <rohit.patnaik@indiapost.gov.in>
2023-11-21 01:09:34 +0530

    DB Connection string port changed to string v0.0.6

--- database/database.go
+++ database/database.go
@@ -17,7 +17,7 @@ func InitDatabase(cfg *config.Config) (*sql.DB, error) {
 	// connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", cfg.Database.User, cfg.Database.Password, cfg.Database.Host, cfg.Database.Port, cfg.Database.Name)
 	//for PostgreSQL
 
-	connectionString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		cfg.Database.Host,
 		cfg.Database.Port,
 		cfg.Database.User,
